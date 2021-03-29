dd97613c46a5e4701666e4dd42472ff7ca13a08c
Hacksore <Hacksore@users.noreply.github.com>
2021-03-29 22:14:21 +0000

    deploy: e025de690c9e00878f72ee2d292bf1c23c8447c1

--- GridSelect.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="react" />
-import "./style.css";
-declare type RegionSelectionProperties = {
-    rows?: number;
-    cols?: number;
-    onRegionUpdate: Function;
-};
-declare const GridSelect: ({ onRegionUpdate, rows, cols, }: RegionSelectionProperties) => JSX.Element;
-export { GridSelect };
