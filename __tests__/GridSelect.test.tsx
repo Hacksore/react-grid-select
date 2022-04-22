037cf13f344f23fbd4ad11f6fffd1f0c296f262c
Sean Boult <996134+Hacksore@users.noreply.github.com>
2022-04-22 08:46:29 -0500

    Add onRegionUpdate test

--- __tests__/GridSelect.test.tsx
+++ __tests__/GridSelect.test.tsx
@@ -36,3 +36,28 @@ test("click last cell & background color updates", async () => {
   // ensure that background color matches what was passed in
   expect(ele).toHaveStyle("background: #4d6cdd");
 });
+
+
+test("clicking cell fires correct onRegionUpdate", async () => {
+  const onClick = jest.fn();
+  const result = render(
+    <GridSelect onRegionUpdate={onClick} cols={5} rows={5} />
+  );
+
+  const firstCell = result.container.querySelector("#cell-0-0");
+  fireEvent.click(firstCell);
+
+  expect(onClick).toHaveBeenCalledWith({
+    width: 1,
+    height: 1,
+  });
+
+  const lastCell = result.container.querySelector("#cell-4-4");
+  fireEvent.click(lastCell);
+
+  expect(onClick).toHaveBeenCalledWith({
+    width: 5,
+    height: 5,
+  });
+
+});
